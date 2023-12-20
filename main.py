1d6b696f0b49cd974ee07112e69e2dfd41d9362c
James Restall <james.restall6@gmail.com>
2023-12-20 21:21:16 +0000

    Finished subject input and looping for each subject

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ window.geometry("500x500")
 
 Subject_entries = []
 date = date.today()
+subject_number = 0
+
+
 
 
 def clearGUI():
@@ -19,23 +22,23 @@ def clearGUI():
 
 
 
-
 def getGeneralInputs():
     clearGUI()
     print("Get General inputs")
 
     input_var = tk.StringVar() #creates empty string variable
     question_prompt = tk.Message(window, text="How many subjects do you take?")
-    number_of_subjects = tk.Entry(window, textvariable=input_var)
+    number_of_subjects_entry = tk.Entry(window, textvariable=input_var)
 
     question_prompt.pack()
-    number_of_subjects.pack()
+    number_of_subjects_entry.pack()
 
-    def submit(number_of_subjects):
+    def submit():
+        global number_of_subjects
         number_of_subjects = int(input_var.get())
-        subject_class_handling(number_of_subjects)
+        subject_class_handling()
 
-    Submit_Button = tk.Button(window, text="Submit", command=lambda: submit(number_of_subjects))
+    Submit_Button = tk.Button(window, text="Submit", command=submit)
     Submit_Button.pack()
 
 
@@ -49,10 +52,14 @@ class subject:
         self.task_counter = 0  #For rest of values, creates empty lists, variables etc.
         self.task_names = []
         self.task_values = []
+        self.task_times = []
+
+        self.block_revision = tk.BooleanVar()
 
 
     def class_input(self):
         clearGUI()
+        task_grid = tk.Frame(window)
 
         #Title
         self.heading = tk.Message(window, text=self.name)
@@ -65,54 +72,67 @@ class subject:
         #Tasks input
         self.task_heading = tk.Message(window, text="Tasks")
 
-        def tasks_add():
-            task_grid = tk.Frame(window)
-            print("task counter = ",self.task_counter)
+        self.task_name_heading = tk.Message(task_grid, text="Name")
+        self.task_name_heading.grid(row=0, column=0)
 
-            task_name_entry = tk.Entry(task_grid)
-            task_name_entry.grid(row=self.task_counter, column=0)
+        self.task_values_heading = tk.Message(task_grid, text="Count")
+        self.task_values_heading.grid(row=0, column=1)
+
+        self.task_times_heading = tk.Message(task_grid, text="Time(mins)")
+        self.task_times_heading.grid(row=0, column=2)
+
+        def tasks_add(): #Adds a row to the task grid
+
+            print("task counter = ", self.task_counter)
+
+            task_name_entry = tk.Entry(task_grid) #Creates entry
+            task_name_entry.grid(row=(self.task_counter)+1, column=0) #Positions the entry on another row in the first column
             self.task_names.append(task_name_entry)
 
             task_value_entry = tk.Entry(task_grid)
-            task_value_entry.grid(row=self.task_counter, column=1)
+            task_value_entry.grid(row=(self.task_counter)+1, column=1) #position in the second column
             self.task_values.append(task_value_entry)
 
-            self.task_counter += 1
+            task_time_entry = tk.Entry(task_grid)
+            task_time_entry.grid(row=(self.task_counter)+1, column=2) #poisitions in the third column
+            self.task_times.append(task_time_entry)
 
-            task_grid.pack()
-            pack_widgets()
+            self.task_counter += 1 #increments task counter
+
+            pack_widgets(task_grid)
 
 
-        self.add_task = tk.Button(window, text="+", command=tasks_add)
+        self.add_task = tk.Button(window, text="+", command=tasks_add) #Creates a button when pressed executes tasks_add function
 
-        print("pack all widgets")
 
-        def pack_widgets():
+        #Way of going through tasks
+        block_revision_radio = tk.Radiobutton(window, text="Block revision (Complete all of one task before starting the next task)", variable=self.block_revision, value=True) #Creates choices which set the self.block_revision variable true or false
+        mixed_revision_radio = tk.Radiobutton(window, text="Mixed revision (Go between different tasks. Start multiple tasks before finishing one)", variable=self.block_revision, value=False)
+
+
+        #Submit button
+        def submit():
+            clearGUI()
+            subject_class_handling()
+
+        submit_button = tk.Button(window, text="Submit", command=submit) #Button that when clicked executes submit function
+
+        def pack_widgets(task_grid):
             self.heading.pack()
             self.line_seperator.pack(fill="x")
             self.question_prompt1.pack()
             self.deadline.pack()
+            self.line_seperator.pack(fill="x")
             self.task_heading.pack()
+
+
+            task_grid.pack()
             self.add_task.pack()
+            block_revision_radio.pack()
+            mixed_revision_radio.pack()
+            submit_button.pack()
 
-        pack_widgets()
-
-    def deadline_input(self):
-        clearGUI()
-
-
-
-        self.submit = tk.Button(window, text="Submit", command=self.tasks_input)
-
-        self.question_prompt.pack()
-        self.deadline.pack()
-        self.submit.pack()
-
-        #self.deadline = int(self.string.get())
-
-
-    def tasks_input(self):
-        clearGUI()
+        pack_widgets(task_grid)
 
 
 
@@ -121,27 +141,48 @@ class subject:
 
 
 
-def subject_class_handling(number_of_subjects):
-    counter = 0
+
+
+
+
+
+
+
+
+
+
+def subject_class_handling():
     subjects = [""] * number_of_subjects
 
-    def create_class(subject_name,counter):
-        subjects[counter] = subject(subject_name)
+    def create_class(subject_name, subject_number):
+        subjects[subject_number] = subject(subject_name)
         print("Create class")
-        subjects[counter].class_input()
+        subjects[subject_number].class_input()
 
     def new_class():
-        if counter < number_of_subjects:
+        global subject_number
+        if subject_number < number_of_subjects:
+            subject_number += 1
+            print("Subject_number = ",subject_number)
             clearGUI()
-            question_prompt = tk.Message(window, text=f"What is the name of subject {counter+1}?")
+            question_prompt = tk.Message(window, text=f"What is the name of subject {subject_number}?")
             subject_name = tk.Entry(window)
-            submit = tk.Button(window, text="Submit", command=lambda: create_class(subject_name.get(),counter))
+            submit = tk.Button(window, text="Submit", command=lambda: create_class(subject_name.get(),subject_number))
 
             question_prompt.pack()
             subject_name.pack()
             submit.pack()
+        else:
+            display_outputs()
+
 
     new_class()
+
+
+
+def display_outputs():
+    print("Display outputs")
+
 
 def controlFunction(): #Main function which calls other functions
     getGeneralInputs()
